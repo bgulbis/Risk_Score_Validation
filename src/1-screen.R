c790c71940809cc83d54f31d5e07ff0992f1df6d
bgulbis <brian.gulbis@gmail.com>
2016-08-16 12:30:48 -0500

    add query details; exclude prison and readmit

--- src/1-screen.R
+++ src/1-screen.R
@@ -2,19 +2,57 @@
 
 library(edwr)
 library(dplyr)
-library(tidyr)
 library(lubridate)
 
 data.raw <- "data/raw"
 
-# patients eligible for inclusion
+# Run EDW query: Patients - by Unit Admission
+#   * Person Location - Nurse Unit (To):
+#       - Cullen 2 E Medical Intensive Care Unit
+#       - HVI Cardiovascular Intensive Care Unit
+#       - HVI Cardiac Care Unit
+#       - Hermann 3 Shock Trauma Intensive Care Unit
+#       - Hermann 3 Transplant Surgical ICU
+#       - Jones 7 J Elective Neuro ICU
+#       - Jones 7 Neuro Trauma ICU
+#   * Person Location- Facility (Curr): Memorial Hermann Hospital
+#   * Admit date range: User-Defined
+#   * Admit begin: 7/1/2014 00:00:00
+#   * Admit end: 7/1/2016 00:00:00
+
 screen <- read_data(data.raw, "screen") %>%
     as.patients() %>%
     filter(discharge.datetime <= ymd("2016-06-30", tz = "US/Central"),
-           age >= 18)
+           age >= 18) %>%
+    arrange(pie.id)
 
-print(concat_encounters(screen$pie.id, 910))
+pie <- concat_encounters(screen$pie.id, 910)
 
-# apply exclusion criteria
+# Run EDW Query: Demographics
+#   * PowerInsight Encounter Id: all values from object pie
 
-# randomly select 100 each before and after Oct 1, 2015 (ICD-10 implementation)
+demograph <- read_data(data.raw, "demographics") %>%
+    as.demographics()
+
+# remove any discharge to court/law
+excl.prison <- demograph %>%
+    filter(disposition %in% c("DC/TF TO COURT/LAW", "Court/Law Enforcement"))
+
+demograph <- anti_join(demograph, excl.prison, by = "pie.id")
+
+# keep only the first admission for any individual patient
+unique.pts <- demograph %>%
+    group_by(person.id) %>%
+    inner_join(screen, by = "pie.id") %>%
+    arrange(person.id, discharge.datetime) %>%
+    summarize(pie.id = first(pie.id))
+
+
+# queries to run: demographics; diagnosis codes; labs - abg, cbc, chemistry,
+# lfts, pregnancy; location history; measures; urine output; ventilator data -
+# settings, start and stop; vitals; icu assessments; surgeries?
+
+# need to add: VBGs; components of GCS
+
+# apply exclusion criteria; randomly select 100 each before and after Oct 1, 2015
+# (ICD-10 implementation)
