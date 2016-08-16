2a9918c76d78610cad24a86fe4d9f58542a5968c
bgulbis <brian.gulbis@gmail.com>
2016-08-16 11:04:56 -0500

    list of potential patients

--- src/1-screen.R
+++ src/1-screen.R
@@ -7,8 +7,14 @@ library(lubridate)
 
 data.raw <- "data/raw"
 
+# patients eligible for inclusion
 screen <- read_data(data.raw, "screen") %>%
     as.patients() %>%
-    filter(discharge.datetime <= ymd("2016-06-30", tz = "US/Central"))
+    filter(discharge.datetime <= ymd("2016-06-30", tz = "US/Central"),
+           age >= 18)
+
+print(concat_encounters(screen$pie.id, 910))
+
+# apply exclusion criteria
 
 # randomly select 100 each before and after Oct 1, 2015 (ICD-10 implementation)
