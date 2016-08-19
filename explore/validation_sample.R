40a9fbfe70236d0ac32abe97ab005e9eaf6f6ff3
Brian Gulbis <brian.gulbis@gmail.com>
2016-08-18 22:01:56 -0500

    get sample of 10 patients to validate

---
+++ explore/validation_sample.R
@@ -0,0 +1,35 @@
+# validation_sample.R
+
+library(edwr)
+library(dplyr)
+
+data.raw <- "data/raw"
+
+include <- readRDS("data/tidy/include.Rds")
+
+icd.codes <- read_data(data.raw, "diagnosis") %>%
+    as.diagnosis() %>%
+    semi_join(include, by = "pie.id")
+
+# separate patients with ICD-9 and ICD-10 codes
+
+set.seed(7708)
+icd9 <- icd.codes %>%
+    filter(code.source == "ICD-9-CM") %>%
+    distinct(pie.id) %>%
+    sample_n(5)
+
+set.seed(12312)
+icd10 <- icd.codes %>%
+    filter(code.source == "ICD-10-CM") %>%
+    distinct(pie.id) %>%
+    sample_n(5)
+
+pts.sample <- bind_rows(icd9, icd10)
+
+pie <- concat_encounters(pts.sample$pie.id)
+print(pie)
+
+# Run EDW Query: Identifiers - by PowerInsight Encounter Id
+#   * PowerInsight Encounter Id: all values from object pie
+
