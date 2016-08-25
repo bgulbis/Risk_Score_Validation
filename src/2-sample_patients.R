265ce03123b56782f8b65f1d6e508601eb308c63
bgulbis <brian.gulbis@gmail.com>
2016-08-25 15:46:44 -0500

    remove patients with both icd9 and icd10 codes

--- src/2-sample_patients.R
+++ src/2-sample_patients.R
@@ -3,37 +3,44 @@
 library(edwr)
 library(dplyr)
 
-data.raw <- "data/raw"
+data_raw <- "data/raw"
 
-include <- readRDS("data/tidy/include.Rds")
+eligible <- readRDS("data/tidy/eligible.Rds")
 
-icd.codes <- read_data(data.raw, "diagnosis") %>%
+icd_codes <- read_data(data_raw, "diagnosis") %>%
     as.diagnosis() %>%
     tidy_data() %>%
-    semi_join(include, by = "pie.id")
+    semi_join(eligible, by = "pie.id") %>%
+    distinct(pie.id, icd9)
+
+excl_icd <- icd_codes %>%
+    group_by(pie.id) %>%
+    summarize(n = n()) %>%
+    filter(n > 1)
+
+icd_codes <- anti_join(icd_codes, excl_icd, by = "pie.id")
 
 # separate patients with ICD-9 and ICD-10 codes and sample evenly from both
 num <- 900
 
 set.seed(7708)
-icd9 <- icd.codes %>%
+icd9 <- icd_codes %>%
     filter(icd9 == TRUE) %>%
     distinct(pie.id) %>%
     sample_n(num)
 
 set.seed(12312)
-icd10 <- icd.codes %>%
+icd10 <- icd_codes %>%
     filter(icd9 == FALSE) %>%
     distinct(pie.id) %>%
     sample_n(num)
 
-pts.sample <- bind_rows(icd9, icd10) %>%
-    arrange(pie.id) %>%
-    distinct()
+pts_sample <- bind_rows(icd9, icd10) %>%
+    arrange(pie.id)
 
-pie <- concat_encounters(pts.sample$pie.id)
+pie <- concat_encounters(pts_sample$pie.id)
 
-saveRDS(pts.sample, "data/tidy/patients_sampled.Rds")
+saveRDS(pts_sample, "data/tidy/patients_sampled.Rds")
 
 # Run EDW Queries:
 #   - DRG Codes - All
