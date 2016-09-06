7fde7d443efda89786df862943e474c1b5aa3606
bgulbis <brian.gulbis@gmail.com>
2016-09-06 10:13:49 -0500

    save list of FINs as csv

--- src/4-manual_patients.R
+++ src/4-manual_patients.R
@@ -2,8 +2,10 @@
 
 library(edwr)
 library(dplyr)
+library(readr)
 
 data.tidy <- "data/tidy"
+data.external <- "data/external"
 
 dirr::get_rds(data.tidy)
 
@@ -13,3 +15,15 @@ print(concat_encounters(patients_sampled$pie.id))
 #   * Identifiers - by PowerInsight Encounter Id
 #       - all values from patients_sampled
 
+identifiers <- read_data(data.external, "^identifiers") %>%
+    as.id()
+
+# create list of FINs for manual review and save to csv
+
+manual_review <- select(icu_admit, pie.id, arrive.datetime, depart.datetime) %>%
+    semi_join(patients_sampled, by = "pie.id") %>%
+    inner_join(identifiers, by = "pie.id") %>%
+    ungroup() %>%
+    select(fin, arrive.datetime, depart.datetime)
+
+write_csv(manual_review, paste(data.external, "manual_review.csv", sep = "/"))
