6dabead21d078ae85ff4150cb7178c59c64d58eb
Brian Gulbis <brian.gulbis@gmail.com>
2016-09-18 16:18:36 -0500

    add comorbidity to saps2

--- src/6-calculate_saps2.R
+++ src/6-calculate_saps2.R
@@ -28,6 +28,26 @@ vent <- tidy_vent_times %>%
     summarize(vent = sum(vent, na.rm = TRUE)) %>%
     mutate(vent = vent >= 1)
 
+tmp_comorbid <- data_comorbidities_icd9 %>%
+    mutate(aids = hiv & (kaposi | lymphoma | pneumocytosis | toxoplasmosis | tuberculosis),
+           heme = leukemia | lymphoma | mult_myeloma) %>%
+    select(pie.id, aids, heme, cancer_mets) %>%
+    gather(comorbidity, val, -pie.id) %>%
+    filter(val == TRUE)
+
+z <- tmp_comorbid$comorbidity
+
+tmp_comorbid$score <- case_when(
+    z == "aids" ~ 3,
+    z == "heme" ~ 2,
+    z == "cancer_mets" ~ 1,
+    is.character(z) ~ 0
+)
+
+saps2_comorbid <- tmp_comorbid %>%
+    arrange(pie.id, score) %>%
+    distinct(pie.id, .keep_all = TRUE)
+
 saps2_test <- inner_join(labs_min_max, vitals_min_max, by = c("pie.id", "min")) %>%
     left_join(data_vent, by = "pie.id") %>%
     left_join(data_gcs, by = "pie.id") %>%
@@ -36,6 +56,7 @@ saps2_test <- inner_join(labs_min_max, vitals_min_max, by = c("pie.id", "min")) 
     left_join(data_surgery[c("pie.id", "elective")], by = "pie.id") %>%
     left_join(vent, by = "pie.id") %>%
     mutate_if(is.character, as.numeric) %>%
+    left_join(saps2_comorbid[c("pie.id", "comorbidity")], by = "pie.id") %>%
     mutate(fio2 = coalesce(fio2, 21),
            admit_type = if_else(elective == FALSE, "elective", "emergency", "nonoperative")) %>%
     select(-dbp, -map, -rr, -spo2, -elective)
