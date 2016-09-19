d5f62f3eb8a99a0be3145562549bc5262b21a72e
bgulbis <brian.gulbis@gmail.com>
2016-09-19 11:08:12 -0500

    make manual dataset for apache3

--- src/8-prepare_apache3.R
+++ src/8-prepare_apache3.R
@@ -1,10 +1,6 @@
 # prepare apache3
 
-source("src/6-calculate_saps2.R")
-
-# labs_apache3 <- bind_rows(lab_min, lab_max) %>%
-#     select(pie.id, min, albuminpco2, ph, pao2, hco3, scr, hct:wbc)
-# select(pie.id, min, albumin, glucose, bili)
+source("src/7-prepare_saps2.R")
 
 gcs_apache3 <- tidy_icu_scores %>%
     inner_join(tmp_icu_stay, by = "pie.id") %>%
@@ -45,6 +41,32 @@ apache3_comorbid <- tmp_comorbid %>%
     arrange(pie.id, score) %>%
     distinct(pie.id, .keep_all = TRUE)
 
+tmp_comorbid <- manual_data %>%
+    spread(comorbidity, value) %>%
+    select(pie.id, aids, hepatic_failure, lymphoma, cancer_mets, leukemia, mult_myeloma, immunosuppress, cirrhosis) %>%
+    gather(comorbidity, val, -pie.id) %>%
+    filter(val == TRUE)
+
+z <- tmp_comorbid$comorbidity
+
+tmp_comorbid$score <- case_when(
+    z == "aids" ~ 6,
+    z == "hepatic_failure" ~ 5,
+    z == "lymphoma" ~ 4,
+    z == "cancer_mets" ~ 3,
+    z == "leukemia" | z == "mult_myeloma" | z == "immunosuppress" ~ 2,
+    z == "cirrhosis" ~ 1,
+    is.character(z) ~ 0
+)
+
+apache3_manual <- tmp_comorbid %>%
+    arrange(pie.id, score) %>%
+    distinct(pie.id, .keep_all = TRUE)
+
+manual_hd <- manual_data %>%
+    filter(comorbidity == "chronic_hd") %>%
+    spread(comorbidity, value)
+
 # need to check for chronic HD
 data_apache3 <- inner_join(labs_min_max, vitals_min_max, by = c("pie.id", "min")) %>%
     left_join(tidy_demographics[c("pie.id", "age")], by = "pie.id") %>%
@@ -54,12 +76,22 @@ data_apache3 <- inner_join(labs_min_max, vitals_min_max, by = c("pie.id", "min")
     left_join(gcs_apache3, by = "pie.id") %>%
     left_join(data_surgery[c("pie.id", "elective")], by = "pie.id") %>%
     mutate_if(is.character, as.numeric) %>%
-    left_join(apache3_comorbid[c("pie.id", "comorbidity")], by = "pie.id") %>%
     mutate(fio2 = coalesce(fio2, 21),
            aa_grad = aa_gradient(pco2, pao2, fio2, F_to_C(temp), 13.106),
-           arf = scr >= 1.5 & uop < 410,
            admit = if_else(elective == FALSE, "elective", "emergency", "nonoperative"))
 
-# saveRDS(gcs_apache3, "data/external/apache3_neuro_test.Rds")
+apache3_icd <- left_join(data_apache3, apache3_comorbid[c("pie.id", "comorbidity")], by = "pie.id") %>%
+    left_join(data_comorbidities_icd9[c("pie.id", "chronic_hd")], by = "pie.id") %>%
+    mutate(arf = scr >= 1.5 & uop < 410 & chronic_hd == FALSE)
 
-score_apache3 <- apache3(data_apache3)
+apache3_man <- left_join(data_apache3, apache3_manual[c("pie.id", "comorbidity")], by = "pie.id") %>%
+    left_join(manual_hd, by = "pie.id") %>%
+    mutate(arf = scr >= 1.5 & uop < 410 & (is.na(chronic_hd) | chronic_hd == FALSE))
+
+score_apache3_icd <- apache3(apache3_icd)
+score_apache3_man <- apache3(apache3_man)
+
+saveRDS(apache3_icd, "data/final/apache3_icd.Rds")
+saveRDS(apache3_man, "data/final/apache3_man.Rds")
+saveRDS(score_apache3_icd, "data/final/score_apache3_icd.Rds")
+saveRDS(score_apache3_man, "data/final/score_apache3_man.Rds")
