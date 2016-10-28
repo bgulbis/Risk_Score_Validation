22a133e05a0726974e90b0b1b3e91274de34a558
bgulbis <brian.gulbis@gmail.com>
2016-10-28 11:13:12 -0500

    add script to make poster figures in pptx

---
+++ src/10-make_poster_figures.R
@@ -0,0 +1,58 @@
+# make_poster_figures
+
+library(tidyverse)
+library(ReporteRs)
+
+dirr::get_rds("../data/final")
+
+scores <- ls(pattern = "score_apache2")
+
+apache2_scores <- map(scores, ~mutate(get(.x), score = .x)) %>%
+    map(~select(.x, pie.id, score, apache2)) %>%
+    map_df(~semi_join(.x, manual_patients, by = "pie.id")) %>%
+    arrange(pie.id, score) %>%
+    dmap_at("score", str_replace_all, pattern = "score_apache2_", replacement = "") %>%
+    mutate(type = "apache2") %>%
+    rename(comorbidity = score, risk_score = apache2)
+
+scores <- ls(pattern = "score_apache3")
+
+apache3_scores <- map(scores, ~mutate(get(.x), score = .x)) %>%
+    map(~select(.x, pie.id, score, apache3)) %>%
+    map_df(~semi_join(.x, manual_patients, by = "pie.id")) %>%
+    arrange(pie.id, score) %>%
+    dmap_at("score", str_replace_all, pattern = "score_apache3_", replacement = "") %>%
+    mutate(type = "apache3") %>%
+    rename(comorbidity = score, risk_score = apache3)
+
+scores <- ls(pattern = "score_saps2")
+
+saps2_scores <- map(scores, ~mutate(get(.x), score = .x)) %>%
+    map(~select(.x, pie.id, score, saps2)) %>%
+    map_df(~semi_join(.x, manual_patients, by = "pie.id")) %>%
+    arrange(pie.id, score) %>%
+    dmap_at("score", str_replace_all, pattern = "score_saps2_", replacement = "") %>%
+    mutate(type = "saps2") %>%
+    rename(comorbidity = score, risk_score = saps2)
+
+risk_scores <- bind_rows(apache2_scores, apache3_scores, saps2_scores)
+
+graph <- ggplot(risk_scores, aes(x = comorbidity, y = risk_score)) +
+    geom_boxplot() +
+    facet_grid(type ~ ., scales = "free_y")
+
+mydoc <- pptx()
+mydoc <- addSlide(mydoc, slide.layout = "Blank")
+mydoc <- addPlot(mydoc,
+                 fun = print,
+                 x = graph,
+                 offx = 1,
+                 offy = 1,
+                 width = 5.5,
+                 height = 5*5.5/7,
+                 vector.graphic = TRUE,
+                 fontname_sans = "Calibri")
+
+# mydoc <- addSlide(mydoc, slide.layout = "Blank")
+
+writeDoc(mydoc, file = "report/ashp_poster_figures.pptx")
